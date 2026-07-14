7ea5101f55730ec689f2052d19bd361a279a4f91
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-14 14:06:25 +0530

    Create 20-ValidParentheses

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,23 @@
+// Last updated: 7/14/2026, 2:06:25 PM
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
