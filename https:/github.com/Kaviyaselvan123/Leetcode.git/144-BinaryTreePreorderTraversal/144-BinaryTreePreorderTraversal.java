606fd7d26a8502cf31ee9489c1f288bce5ffc5cd
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-14 14:05:52 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -0,0 +1,27 @@
+// Last updated: 7/14/2026, 2:05:52 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> ans=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root!=null){
+            ans.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return ans;
+    }
+}
