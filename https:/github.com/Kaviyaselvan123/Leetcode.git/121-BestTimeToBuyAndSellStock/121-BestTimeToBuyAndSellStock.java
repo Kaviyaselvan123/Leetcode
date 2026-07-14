1bca79b0047ad9f567a2173cdeaf67b09ab79eb4
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-14 14:05:59 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:05:59 PM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int price : prices) {   
+            if(price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }  
+        return maxProfit;
+    }
+}
