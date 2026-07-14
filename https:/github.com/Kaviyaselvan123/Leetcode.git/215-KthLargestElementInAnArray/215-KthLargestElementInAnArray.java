67d4bb0eb934466b311fd9d8f613d67aef26f77b
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-14 14:05:42 +0530

    Create 215-KthLargestElementInAnArray

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 2:05:42 PM
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
