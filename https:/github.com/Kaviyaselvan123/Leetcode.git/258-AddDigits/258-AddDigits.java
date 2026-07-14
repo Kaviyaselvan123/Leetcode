ba26be7daeca1c5b9d8da6d36efc95fb1e112e00
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-14 14:05:32 +0530

    Create 258-AddDigits

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/258-AddDigits/258-AddDigits.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:05:32 PM
+class Solution {
+    public int addDigits(int num) {
+        while(num>9){
+            int s=0;
+            while(num>0){
+               s+=num%10;
+               num=num/10;
+            }
+            num=s; 
+        }
+        return num;
+    }
+}
