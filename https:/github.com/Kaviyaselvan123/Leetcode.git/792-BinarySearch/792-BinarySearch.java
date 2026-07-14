e1ef79ffdf44dbc274762b49894a016fe3ffa8c9
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-14 14:04:59 +0530

    Create 792-BinarySearch

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 2:04:59 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) r=mid-1;
+            else l=mid+1;
+        }
+        return -1;
+    }
+}
