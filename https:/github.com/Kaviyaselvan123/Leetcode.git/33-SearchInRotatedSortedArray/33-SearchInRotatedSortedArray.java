659a9256349707d7504ff00d57b309394633844a
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-31 11:43:02 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.java
@@ -0,0 +1,26 @@
+// Last updated: 7/31/2026, 11:43:03 AM
+class Solution {
+    public int search(int[] nums, int target) {
+    int l=0,h=nums.length-1;
+    while(l<=h){
+        int m=l+(h-l)/2;
+        if(nums[m]==target){
+            return m;
+        }
+        if(nums[l]<=nums[m]){
+            if(target>=nums[l]&&target<nums[m]){
+                h=m-1;
+            }else{
+                l=m+1;
+            }
+        }else{
+            if(target>nums[m]&&target<=nums[h]){
+                l=m+1;
+            }else{
+                h=m-1;
+            }
+        }
+    }  
+    return -1;  
+    }
+}
