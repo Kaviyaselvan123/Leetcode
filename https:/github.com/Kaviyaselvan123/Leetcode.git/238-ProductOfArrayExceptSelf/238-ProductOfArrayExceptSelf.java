920283690fed5400f491d3105dc12aba5dd7620f
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-31 11:41:41 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2026, 11:41:42 AM
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+     int p[]=new int[nums.length];
+     int s[]=new int[nums.length];
+     p[0]=1;
+     s[nums.length-1]=1;
+     for(int i=1;i<nums.length;i++){
+        p[i]=p[i-1]*nums[i-1];
+     }
+     for(int i=nums.length-2;i>=0;i--){
+        s[i]=s[i+1]*nums[i+1];
+     }
+     for(int i=0;i<nums.length;i++){
+        p[i]=p[i]*s[i];
+     }
+     return p;
+    }
+}
