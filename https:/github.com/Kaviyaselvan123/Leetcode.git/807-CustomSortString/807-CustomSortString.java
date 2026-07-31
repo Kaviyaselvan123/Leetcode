b9b8836a772c5e83dffc242de1f796f57db062b2
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-07-31 11:40:25 +0530

    Create 807-CustomSortString

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/807-CustomSortString/807-CustomSortString.java
@@ -0,0 +1,23 @@
+// Last updated: 7/31/2026, 11:40:26 AM
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (char c : order.toCharArray()) {
+            while (freq[c - 'a'] > 0) {
+                ans.append(c);
+                freq[c - 'a']--;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 0) {
+                ans.append((char)('a' + i));
+                freq[i]--;
+            }
+        }
+        return ans.toString();
+    }
+}
