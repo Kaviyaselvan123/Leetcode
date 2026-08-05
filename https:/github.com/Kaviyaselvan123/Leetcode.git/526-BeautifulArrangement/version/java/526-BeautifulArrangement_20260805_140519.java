06426a5f376c9bfac0251f6b1d5ab9674090ce49
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-08-05 14:05:18 +0530

    Create 526-BeautifulArrangement

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/526-BeautifulArrangement/version/java/526-BeautifulArrangement_20260805_140519.java
@@ -0,0 +1,22 @@
+// Last updated: 8/5/2026, 2:05:19 PM
+1class Solution {
+2    int count = 0;
+3    public int countArrangement(int n) {
+4        boolean[] used = new boolean[n + 1];
+5        backtrack(1, n, used);
+6        return count;
+7    }
+8    private void backtrack(int pos, int n, boolean[] used) {
+9        if (pos > n) {
+10            count++;
+11            return;
+12        }
+13        for (int num = 1; num <= n; num++) {
+14            if (!used[num] && (num % pos == 0 || pos % num == 0)) {
+15                used[num] = true;
+16                backtrack(pos + 1, n, used);
+17                used[num] = false;
+18            }
+19        }
+20    }
+21}
