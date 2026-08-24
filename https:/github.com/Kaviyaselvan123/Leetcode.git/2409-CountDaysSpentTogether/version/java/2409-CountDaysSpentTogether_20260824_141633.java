f1918d05ad18dbe6f9fdb1b99f6ce2f254397e81
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-08-24 14:16:29 +0530

    Create 2409-CountDaysSpentTogether

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/2409-CountDaysSpentTogether/version/java/2409-CountDaysSpentTogether_20260824_141633.java
@@ -0,0 +1,22 @@
+// Last updated: 8/24/2026, 2:16:33 PM
+1class Solution {
+2    public String makeGood(String s) {
+3        Stack<Character> st=new Stack<>();
+4        for(int i=0;i<s.length();i++){
+5            char ch=s.charAt(i);
+6            if(st.isEmpty()){
+7                st.push(ch);
+8            }else if((Character.isUpperCase(ch)&&Character.isLowerCase(st.peek())||Character.isUpperCase(st.peek())&&Character.isLowerCase(ch))&&(Character.toLowerCase(ch)==Character.toLowerCase(st.peek()))){
+9                    st.pop();
+10            }else{
+11                System.out.println(ch);
+12                st.push(ch);
+13            }
+14        }
+15        String si="";
+16        for(char x:st){
+17            si+=x;
+18        }
+19        return si;
+20    }
+21}
