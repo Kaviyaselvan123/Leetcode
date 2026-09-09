70d4a3d8cbca2cefe65f88412fb3542e249ff092
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-09-09 16:16:59 +0530

    Create 541-ReverseStringII

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/541-ReverseStringII/version/java/541-ReverseStringII_20260909_161659.java
@@ -0,0 +1,18 @@
+// Last updated: 9/9/2026, 4:16:59 PM
+1class Solution {
+2    public String reverseStr(String s, int k) {
+3    char[] b=s.toCharArray();
+4    for(int i=0;i<b.length;i+=2*k){
+5        int l=i;
+6        int r=Math.min(i+k-1,b.length-1);
+7        while(l<r){
+8            char temp=b[l];
+9            b[l]=b[r];
+10            b[r]=temp;
+11            l++;
+12            r--;
+13        }
+14    }
+15        return new String(b); 
+16    }
+17}
