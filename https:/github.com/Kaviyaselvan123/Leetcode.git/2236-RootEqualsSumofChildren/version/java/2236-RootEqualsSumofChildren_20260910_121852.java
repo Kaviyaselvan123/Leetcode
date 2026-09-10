8049f62e4eba32b1263ee0cf899728567240d934
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-09-10 12:18:48 +0530

    Create 2236-RootEqualsSumofChildren

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/2236-RootEqualsSumofChildren/version/java/2236-RootEqualsSumofChildren_20260910_121852.java
@@ -0,0 +1,22 @@
+// Last updated: 9/10/2026, 12:18:52 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean checkTree(TreeNode root) {
+18        if(root.left.val+root.right.val==root.val) return true;
+19        return false;
+20    }
+21}
