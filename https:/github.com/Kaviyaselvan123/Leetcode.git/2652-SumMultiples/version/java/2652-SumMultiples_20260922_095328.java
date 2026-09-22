d79d0f17d348605bd923394f40ac5e5c21771235
Kaviyaselvan M <145749617+Kaviyaselvan123@users.noreply.github.com>
2026-09-22 09:53:24 +0530

    Create 2652-SumMultiples

---
+++ https:/github.com/Kaviyaselvan123/Leetcode.git/2652-SumMultiples/version/java/2652-SumMultiples_20260922_095328.java
@@ -0,0 +1,14 @@
+// Last updated: 9/22/2026, 9:53:28 AM
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum = sum + i;
+            }
+        }
+
+        return sum;
+    }
+}
